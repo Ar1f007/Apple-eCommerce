af6c2bc628c47dacff85d3c5adaf610187e641d9
Ar1f007 <muhammadarif9416@yahoo.com>
2021-04-28 00:01:44 +0600

    MacBook Air section done

--- script.js
+++ script.js
@@ -90,3 +90,16 @@ document.querySelector(".controls").addEventListener("mouseout", () => {
 
 playPause();
 // End of Cube
+
+// Section 3
+const section3Content = document.querySelector(".section-3-content");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.pageYOffset + window.innerHeight >=
+    section3Content.offsetTop + section3Content.offsetHeight / 2
+  ) {
+    section3Content.classList.add("change");
+  }
+});
+// End of Section 3
